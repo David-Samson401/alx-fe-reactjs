e7c13cfe42043c2368b6805130f636e73e2ed6e1
David Ogik <divokorino284@gmail.com>
2025-07-19 14:00:03 +0300

    Apply inline styling to components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+
+
+  return (
+    <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+} 
---
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -0,0 +1,11 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I'm excited to build more complex applications with React!I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;

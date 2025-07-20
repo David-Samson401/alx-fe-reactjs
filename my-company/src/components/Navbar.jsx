51189f5850851fc82be1a83568a9ad1d6e027d0b
David Ogik <divokorino284@gmail.com>
2025-07-20 16:49:25 +0300

    Add my-company React project with routing and styling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: 'navy', padding: '10px' }}>
+    <nav style={{ backgroundColor: 'navy', padding: '10px', display: 'flex', justifyContent: 'right' }}>
       <Link to="/" style={{ color: 'white', marginRight: '15px' }}>Home</Link>
       <Link to="/about" style={{ color: 'white', marginRight: '15px' }}>About</Link>
       <Link to="/services" style={{ color: 'white', marginRight: '15px' }}>Services</Link>

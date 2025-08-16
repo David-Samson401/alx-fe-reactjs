83397b809911972a07056dff5342b4d483ba3944
David Ogik <divokorino284@gmail.com>
2025-08-16 16:04:36 +0300

    commit all changes

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-500">
-        Recipe Sharing Platform 🚀
-      </h1>
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default App;
-

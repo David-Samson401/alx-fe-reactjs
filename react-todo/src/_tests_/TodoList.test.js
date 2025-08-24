90efbdae35633cc99ea41de7e6bba79c43b321d2
David Ogik <divokorino284@gmail.com>
2025-08-24 13:12:00 +0300

    set up react-router-advanced project with Vite and React Router

---
+++ react-todo/src/_tests_/TodoList.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+test("renders initial todos", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build a project")).toBeInTheDocument();
+  expect(screen.getByText("Write tests")).toBeInTheDocument();
+});
+
+test("can add a new todo", () => {
+  render(<TodoList />);
+  fireEvent.change(screen.getByPlaceholderText("Add a new todo"), {
+    target: { value: "New Todo" },
+  });
+  fireEvent.click(screen.getByText("Add"));
+  expect(screen.getByText("New Todo")).toBeInTheDocument();
+});
+
+test("can toggle a todo", () => {
+  render(<TodoList />);
+  const todo = screen.getByText("Learn React");
+  fireEvent.click(todo);
+  expect(todo).toHaveStyle("text-decoration: line-through");
+});
+
+test("can delete a todo", () => {
+  render(<TodoList />);
+  const todo = screen.getByText("Learn React");
+  const deleteButton = todo.nextSibling;
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+});

2dacddabca937e12d162f3f7e1590010cafb3c0f
David Ogik <divokorino284@gmail.com>
2025-08-26 11:19:04 +0300

    Fixed code on the TodoList.jsx

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-const TodoList = ({ todos, toggleTodo, deleteTodo }) => {
+function TodoList() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Write Tests", completed: true },
+  ]);
+
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <ul>
-      {todos.map((todo, idx) => (
-        <li
-          key={idx}
-          onClick={() => toggleTodo(idx)} // pass index
-          style={{ textDecoration: "none", cursor: "pointer" }}
-        >
-          {todo.text}
-          <button
-            onClick={(e) => {
-              e.stopPropagation();
-              deleteTodo(idx); // pass index
-            }}
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => toggleTodo(todo.id)}
+          />
+          <span
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
           >
-            Delete
-          </button>
+            {todo.text}
+          </span>
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
         </li>
       ))}
     </ul>
   );
-};
+}
 
 export default TodoList;
